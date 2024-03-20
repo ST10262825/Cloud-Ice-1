23753f665043e0f6f5112f1c76dc6463b13a8810
ST10262825 <128587768+ST10262825@users.noreply.github.com>
2024-03-20 22:45:15 +0200

    Add files via upload

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VehicleApp.Data;
+using VehicleApp.Models;
+
+namespace VehicleApp.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        private readonly VehicleAppContext _context;
+
+        public VehiclesController(VehicleAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vehicles
+        public async Task<IActionResult> Index(string vehicleModel, string searchString)
+        {
+            if (_context.Vehicle == null)
+            {
+                return Problem("Entity set 'VehicleAppContext.Vehicle'  is null.");
+            }
+
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Vehicle
+                                            orderby m.Model
+                                            select m.Model;
+            var vehicles = from m in _context.Vehicle
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                vehicles = vehicles.Where(s => s.Name!.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(vehicleModel))
+            {
+                vehicles = vehicles.Where(x => x.Model == vehicleModel);
+            }
+
+            var vehicleModelVM = new VehicleModelViewModel
+            {
+                Models = new SelectList(await genreQuery.Distinct().ToListAsync()),
+               Vehicles = await vehicles.ToListAsync()
+            };
+
+            return View(vehicleModelVM);
+        }
+
+        // GET: Vehicles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = await _context.Vehicle
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vehicles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,buyDate,Model,Price")] Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vehicle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = await _context.Vehicle.FindAsync(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            return View(vehicle);
+        }
+
+        // POST: Vehicles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,buyDate,Model,Price")] Vehicle vehicle)
+        {
+            if (id != vehicle.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehicle);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleExists(vehicle.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = await _context.Vehicle
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicle);
+        }
+
+        // POST: Vehicles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vehicle = await _context.Vehicle.FindAsync(id);
+            if (vehicle != null)
+            {
+                _context.Vehicle.Remove(vehicle);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleExists(int id)
+        {
+            return _context.Vehicle.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/VehicleAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VehicleApp.Models;
+
+namespace VehicleApp.Data
+{
+    public class VehicleAppContext : DbContext
+    {
+        public VehicleAppContext (DbContextOptions<VehicleAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VehicleApp.Models.Vehicle> Vehicle { get; set; } = default!;
+    }
+}
